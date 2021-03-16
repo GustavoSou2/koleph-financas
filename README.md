3ab3f0f750aeff34c1f83ac37fc129246f08e0a0
GustavoSou2 <gustavosocial1032@gmail.com>
2021-03-16 09:46:06 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+<h1>Koleph Finanças</h1>
+
+
+Projeto de site voltado para finanças e controle de dinhero
---
+++ view/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="author" content="Gustavo Antonio de Souza" />
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <meta name="copyright" content="© 2020 Gustavo de Souza" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="./layout/style/indexStyle.css">
+    <title>Koleph | Finanças</title>
+</head>
+
+<body>
+    <!-- Header -->
+    <header>
+        <!-- navbar > menu -->
+        <i class="fa fa-bars"></i>
+        <nav class="menu">
+            <div id="container-menu">
+                <ul id="menu-content">
+                    <li class="item-menu"><a href="#" class="link-item-menu">Perfil</a></li>
+                    <li class="item-menu"><a href="#" class="link-item-menu">Início</a></li>
+                    <li class="item-menu"><a href="#" class="link-item-menu">Contas</a></li>
+                    <li class="item-menu"><a href="#" class="link-item-menu">Planos</a></li>
+                    <li class="item-menu"><a href="#" class="link-item-menu">Relatório</a></li>
+                    <li class="item-menu"><a href="#" class="link-item-menu">Sair</a></li>
+                </ul>
+            </div>
+        </nav>
+
+        <!-- container logo --left -->
+        <div id="container-logo">
+            <a href="#" id="logo">Markdown</a>
+        </div>
+
+        <!-- container name user -->
+        <div id="container-name-user">
+            <span id="name-content-user">Cláudio</span>
+        </div>
+    </header>
+    <div id="container-element-content">
+        <main>
+            <section id="content-elements">
+                <div id="overflow-x-container-cards">
+                    <div id="container-cards-categ">
+                        <div class="card desp">
+                            <span class="add-account">
+                                <i class="fa fa-plus"></i>Adicionar despesa
+                            </span>
+                        </div>
+                        <div class="card dep">
+                            <span class="add-account">
+                                <i class="fa fa-level-up"></i>Adicionar deposito
+                            </span>
+                        </div>
+                        <div class="card deb">
+                            <span class="add-account">
+                                <i class="fa fa-level-down"></i>Debitar deposito
+                            </span>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+        </main>
+        <aside>
+        </aside>
+    </div>
+</body>
+
+</html>
